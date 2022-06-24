3b5c25e73e6026a6678137670490fb4a2211d137
Piotr Wnuczek <piotrantoniwnuczek@gmail.com>
2022-06-24 14:36:40 +0200

    refactor

--- src/store/appReducer.js
+++ src/store/appReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { firestoreReducer } from 'redux-firestore';
 import { firebaseReducer } from 'react-redux-firebase';
 import usersReducer from 'store/usersReducer';
+import itemsReducer from 'store/usersReducer';
 
 const appReducer = combineReducers({
   users: usersReducer,
+  items: itemsReducer,
   firebase: firebaseReducer,
   firestore: firestoreReducer,
 });
